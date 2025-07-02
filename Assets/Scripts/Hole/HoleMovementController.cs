5edcd4046ead685fe73d255b6c004df9e63bb0d7
Edwin Game Dev <edwinjonesholanda@gmail.com>
2025-07-02 14:19:52 +0100

    Create basic holemovement

---
+++ Assets/Scripts/Hole/HoleMovementController.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+public class HoleMovementController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5;
+    
+    public event Action OnMove;
+    private Vector3 lastPos;
+    
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 moveDir = new(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if (moveDir == Vector3.zero)
+        {
+            return;
+        }
+
+        transform.position += moveDir * (speed * Time.deltaTime);
+        OnMove?.Invoke();
+
+        transform.hasChanged = false;
+    }
+}
