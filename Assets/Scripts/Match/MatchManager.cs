17b4b20f7fb2320b0139dc2efa46c5951632c02c
Edwin Game Dev <edwinjonesholanda@gmail.com>
2025-07-02 14:22:46 +0100

    Create match manager to handle food collision, generate mesh and spawn new hole

---
+++ Assets/Scripts/Match/MatchManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class MatchManager : MonoBehaviour
+{
+    // Match
+    [Header("Match")]
+    [SerializeField] private Collider groundCollider;
+    [SerializeField] private GameObject holePrefab;
+    [SerializeField] private CameraFollow cameraFollow;
+    
+    [Header("Mesh")]
+    [SerializeField] private PolygonCollider2D hole2DCollider;
+    [SerializeField] private PolygonCollider2D ground2DCollider;
+    [SerializeField] private MeshCollider generatedMeshCollider;
+    [SerializeField] private MeshFilter generatedMeshFilter;
+
+    private MeshGenerator meshGenerator;
+
+    private void Start()
+    {
+        meshGenerator = new MeshGenerator(hole2DCollider, ground2DCollider, generatedMeshCollider, generatedMeshFilter);
+        
+        CreateNewHole();
+        DisableFoodCollision();
+
+        meshGenerator.Make3DMeshCollider();
+    }
+
+    private void CreateNewHole()
+    {
+        GameObject holeGameObject = Instantiate(holePrefab);
+        Hole hole = holeGameObject.GetComponentInChildren<Hole>();
+        hole.Setup(groundCollider, generatedMeshCollider);
+        hole.OnIncreaseHoleSize += meshGenerator.UpdateHoleMesh;
+        
+        cameraFollow.SetTarget(holeGameObject.transform);
+        
+    }
+
+    private void DisableFoodCollision()
+    {
+        Food[] foodObjects = FindObjectsOfType<Food>();
+
+        foreach (Food food in foodObjects)
+        {
+            Collider foodCollider = food.GetComponent<Collider>();
+            if (foodCollider == null)
+            {
+                continue;
+            }
+
+            Physics.IgnoreCollision(foodCollider, generatedMeshCollider, true);
+        }
+    }
+}
