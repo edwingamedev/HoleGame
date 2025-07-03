43cf25caa85c8d7a7b408e36b3ab7857e3154c59
Edwin Game Dev <edwinjonesholanda@gmail.com>
2025-07-03 12:04:27 +0100

    Add touch movement

--- Assets/Scripts/Gameplay/HoleMovementController.cs
+++ Assets/Scripts/Gameplay/HoleMovementController.cs
@@ -10,7 +10,11 @@ public class HoleMovementController : MonoBehaviour
     
     private void Update()
     {
-        Move();
+#if UNITY_EDITOR || UNITY_STANDALONE
+        MoveWithKeyboard();
+#else
+        MoveWithTouch();
+#endif
     }
 
     public void IncreaseSpeed()
@@ -18,7 +22,7 @@ public class HoleMovementController : MonoBehaviour
         speed += 1f;
     }
     
-    private void Move()
+    private void MoveWithKeyboard()
     {
         Vector3 moveDir = new(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
@@ -31,5 +35,47 @@ public class HoleMovementController : MonoBehaviour
         OnMove?.Invoke();
 
         transform.hasChanged = false;
+    }
+    
+    private Vector2 touchStartPos;
+    private bool isTouching;
+    
+    private void MoveWithTouch()
+    {
+        if (Input.touchCount == 0)
+            return;
+
+        Touch touch = Input.GetTouch(0);
+
+        switch (touch.phase)
+        {
+            case TouchPhase.Began:
+                touchStartPos = touch.position;
+                isTouching = true;
+                break;
+
+            case TouchPhase.Moved:
+            case TouchPhase.Stationary:
+                if (!isTouching)
+                    return;
+
+                Vector2 touchDelta = touch.position - touchStartPos;
+
+                // Convert screen delta to world movement direction
+                Vector3 moveDir = new Vector3(touchDelta.x, 0, touchDelta.y).normalized;
+
+                transform.position += moveDir * (speed * Time.deltaTime);
+                OnMove?.Invoke();
+                transform.hasChanged = false;
+
+                // Optional: update start pos to make it more like dragging
+                touchStartPos = touch.position;
+                break;
+
+            case TouchPhase.Ended:
+            case TouchPhase.Canceled:
+                isTouching = false;
+                break;
+        }
     }
 }
