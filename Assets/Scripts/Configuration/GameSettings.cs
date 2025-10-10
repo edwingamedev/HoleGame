d7afa1732c6dce9bb8fc48a427d412d736e299ac
Edwin Game Dev <edwinjonesholanda@gmail.com>
2025-10-10 10:07:17 +0100

    Add summary to SelectedLevel

--- Assets/Scripts/Configuration/GameSettings.cs
+++ Assets/Scripts/Configuration/GameSettings.cs
@@ -6,5 +6,10 @@ public class GameSettings : ScriptableObject
 {
     [SerializeField]
     public LevelController[] Levels;
+    /// <summary>
+    /// This variable value is changed on purpose, I'm using it so we can fast prototype the levels.
+    /// Another way of doing it would involve saving on player prefs, static class or even a whole system for being able
+    /// to access and select the level anywhere. 
+    /// </summary>
     public int selectedLevel;
 }
